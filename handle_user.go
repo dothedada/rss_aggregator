dfb413a2cd253e63203cf71219992a591409e815
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-08-07 19:40:30 -0500

    tidy up some user functions

---
+++ handle_user.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/dothedada/rss_aggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handlerRegister(s *State, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("Usage: %s <name>", cmd.name)
+	}
+
+	ctx := context.Background()
+	userData := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+
+	user, err := s.db.CreateUser(ctx, userData)
+	if err != nil {
+		return error(err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("New user created:")
+	printUser(user)
+
+	return nil
+}
+
+func handlerLogin(s *State, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("Usage: %s <name>", cmd.name)
+	}
+
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		log.Fatalf("Couldn't finf the user '%s'", cmd.args[0])
+	}
+
+	if err := s.cfg.SetUser(user.Name); err != nil {
+		return err
+	}
+
+	fmt.Printf("Hello %s, welcome to your feed\n", cmd.args[0])
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf("Name:   %s\n", user.Name)
+	fmt.Printf("ID: 	%s\n", user.ID)
+	fmt.Printf("at: 	%s\n", user.CreatedAt)
+}
